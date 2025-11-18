9bc2253c7709e924a5f6c7ebea1856b2dd94901b
userfrom <kimchonggu04@gmail.com>
2025-11-18 23:12:31 +0700

    Fix: use internal Railway MySQL connection

---
+++ fonend/frontend/src/pages/axiosClient.js
@@ -0,0 +1,18 @@
+// src/api/axiosClient.js
+import axios from "axios";
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Thêm token tự động
+client.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default client;
