e23f27622751b9ca4fcfa4b55cf27de0c4134835
userfrom <kimchonggu04@gmail.com>
2025-11-19 16:23:39 +0700

    Fix Render port binding

--- fonend/frontend/src/api/axiosClient.js
+++ fonend/frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL+ "/api",
   headers: {
     "Content-Type": "application/json",
   },
